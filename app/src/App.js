3d4b11169df2eba69662ad6753af6007feb17472
DakotaWBaker <dakota.baker@student.awesomeinc.org>
2022-12-02 01:05:01 +0000

    added grand exchange items and searching working

--- app/src/App.js
+++ app/src/App.js
@@ -3,18 +3,43 @@ import './App.css';
 import TwitterFeed from './TwitterFeed';
 import Navbar from './Navbar';
 import { useState, useEffect } from 'react'
+import axios from "axios";
 import Home from './Home';
 import GroupFinder from './GroupFinder';
+import GrandExchange from './GrandExchange';
 
-
+// const counter = 1
 
 function App() {
   const [page, setPage] = useState("Home");
+  const [ge, setGe] = useState([])
+  const [pagination, setPagination] = useState(1)
+  const [searchItems, setSearchItems] = useState('')
+  const api = `https://8000-dakotawbake-runelinkapi-zemha1opeck.ws-us77.gitpod.io/api/grandexchange?page=${pagination}`
+  useEffect(() => {
+    async function getData() {
+      const response = await axios.get(api);
+      setGe(response.data);
+    }
+    getData();
+  }, [pagination]);
+  async function getSearchData(searchParam) {
+   
+    const searchResponse = await axios.get(`https://8000-dakotawbake-runelinkapi-zemha1opeck.ws-us77.gitpod.io/api/gesearch?item=${searchParam}`);
+    setSearchItems(searchResponse.data.item)
+    console.log(searchItems)
+  }
   if (page === 'Home') {
   return (
     <>
     <Navbar
     page = {setPage}
+    />
+    <GrandExchange
+    items = {ge}
+    pagination = {pagination}
+    setPage = {setPagination}
+    getSearchData = {getSearchData}
     />
     <Home />
     </>
